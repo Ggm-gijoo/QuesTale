fffbe3acf3059a936a8e97b1b85117c22a35a9e0
Ggm-gijoo <gijoo1012@gmail.com>
2022-05-20 13:46:52 +0900

    2022/05/20
    UI 부분 수정
    ENEMY 태그를 가졌으면 자동으로 전투

--- QuesTale/Assets/01_Scripts/GameManager.cs
+++ QuesTale/Assets/01_Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 public class GameManager : MonoSingleton<GameManager>
 {
@@ -9,6 +10,8 @@ public class GameManager : MonoSingleton<GameManager>
     private GameObject skillPanel;
     [SerializeField]
     private GameObject actPanel;
+    [SerializeField]
+    private Text TurnText;
 
     public List<CharacterManager> characters = new List<CharacterManager>();
 
@@ -17,6 +20,7 @@ public class GameManager : MonoSingleton<GameManager>
 
     void Start()
     {
+        TurnText.text = $"{characters[index].name}의 턴";
         characterCount = characters.Count;
         characters = characters.OrderByDescending(n => n.StatusSpd).ToList();
     }
@@ -32,7 +36,7 @@ public class GameManager : MonoSingleton<GameManager>
     {
         if(characters[index].apNow <= 0 || characters[index].IsDefence) //턴을 넘기는 조건, Ap가 0이 되거나 방어를 했을 때
         {
-            Debug.Log("턴 넘어감!");
+            print("턴 넘어감!");
             if (index + 1 >= characterCount)
                 index = 0;
             else
@@ -42,17 +46,32 @@ public class GameManager : MonoSingleton<GameManager>
 
             if(characters[index].apNow < 9)
             characters[index].apNow++;
+
+
+            TurnText.text = $"{characters[index].name}의 턴";
+            if (characters[index].CompareTag("Player"))
+            {
+                actPanel.SetActive(true);
+                skillPanel.SetActive(false);
+            }
+            else if (characters[index].CompareTag("Enemy"))
+            {
+                actPanel.SetActive(false);
+                skillPanel.SetActive(false);
+                StartCoroutine(EnemyAct());
+            }
         }
     }
 
     public void Act() //방어 외의 행동을 할 때
     {
         --characters[index].apNow;
-        Debug.Log("현재 플레이어 : " + characters[index].name + ", 플레이어의 직업군 : " + characters[index].Status.name + ", 남은 AP : " + characters[index].apNow);
+        print("현재 플레이어 : " + characters[index].name + ", 플레이어의 직업군 : " + characters[index].Status.name + ", 남은 AP : " + characters[index].apNow);
     }
 
     public void Attack() //감안해야 할 것 : 인덱스 + 1의 값이 캐릭터의 수를 넘어가는가? 공격을 받는 상대가 방어를 하고 있는가?
     {
+        print(characters[index].name + "공격!");
         Act();
         if (index + 1 < characterCount)
         {
@@ -88,7 +107,34 @@ public class GameManager : MonoSingleton<GameManager>
 
     public void Defence()
     {
+        print(characters[index].name + "는 방어를 시도했다!");
         characters[index].IsDefence = true;
+        StopCoroutine(EnemyAct());
         TurnChange();
     }
+
+    public IEnumerator EnemyAct()
+    {
+        while(characters[index].apNow >= 0 || !characters[index].IsDefence)
+        {
+            if (characters[index].CompareTag("Enemy"))
+            {
+                yield return new WaitForSeconds(1f);
+                switch (Random.Range(0, 2))
+                {
+                    case 0:
+                        Attack();
+                        break;
+                    case 1:
+                        Defence();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else yield break;
+        }
+        
+    }
+
 }
