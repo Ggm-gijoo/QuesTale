848e5d73108c16878617856e0cb17452a7dc80de
Ggm-gijoo <gijoo1012@gmail.com>
2022-05-24 17:53:14 +0900

    2022/05/24
    스킬에 스크립터블 오브젝트 이용해서 소모 MP, AP, 공격력 추가

---
+++ QuesTale/Assets/01_Scripts/ScriptableObject/SkillStatus.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "스킬", fileName = "스킬")]
+
+public class SkillStatus : ScriptableObject
+{
+    [SerializeField]
+    private float s_power;
+
+    [SerializeField]
+    private float s_useMp;
+
+    [SerializeField]
+    private int s_useAp = 1;
+
+    public float Power { get { return s_power; } set { s_power = value; } }
+
+    public float UseMp { get { return s_useMp; } set { s_useMp = value; } }
+
+    public int UseAp { get { return s_useAp; } set { s_useAp = value; } }
+
+}
