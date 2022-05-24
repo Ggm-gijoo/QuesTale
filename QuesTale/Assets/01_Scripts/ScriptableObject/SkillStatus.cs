d757c42cfcd2c492dc69db0936a752eaa9f1be31
Ggm-gijoo <gijoo1012@gmail.com>
2022-05-25 01:56:15 +0900

    2022/05/25
    Hello

--- QuesTale/Assets/01_Scripts/ScriptableObject/SkillStatus.cs
+++ QuesTale/Assets/01_Scripts/ScriptableObject/SkillStatus.cs
@@ -13,11 +13,16 @@ public class SkillStatus : ScriptableObject
     private float s_useMp;
 
     [SerializeField]
+    private float s_critPercent;
+
+    [SerializeField]
     private int s_useAp = 1;
 
     public float Power { get { return s_power; } set { s_power = value; } }
 
     public float UseMp { get { return s_useMp; } set { s_useMp = value; } }
+
+    public float CritP { get { return s_critPercent; } set { s_critPercent = value; } }
 
     public int UseAp { get { return s_useAp; } set { s_useAp = value; } }
 
